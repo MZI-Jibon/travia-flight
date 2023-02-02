f4943f8d1725dabc53736e3b42f4fd4904e6a28a
UiHut-Jibon <113233436+UiHut-Jibon@users.noreply.github.com>
2023-02-02 23:12:50 +0600

    nice select bakit

--- js/main.js
+++ js/main.js
@@ -18,16 +18,6 @@ $(document).ready(function () {
     $('.tab-content').hide();
     $('.tab-content:first').show();
 
-    // Click function
-    $('#tabs-nav li').click(function () {
-        $('#tabs-nav li').removeClass('active');
-        $(this).addClass('active');
-        $('.tab-content').hide();
-
-        var activeTab = $(this).find('a').attr('href');
-        $(activeTab).fadeIn();
-        return false;
-    });
 
     //JQUERY NICE SELECT JS
     $('select').niceSelect();
